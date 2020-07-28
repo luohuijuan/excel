7f0c780f98c372c04405c4688124e0109e9cc92c
chenyi <776738764@qq.com>
2020-07-28 11:18:57 +0800

    5

--- src/main/java/com/excel/controller/DataController.java
+++ src/main/java/com/excel/controller/DataController.java
@@ -1,10 +1,22 @@
 package com.excel.controller;
 
+import com.excel.mapper.DataMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+import java.util.Map;
 
 @Controller
 public class DataController {
 
+    @Autowired
+    private DataMapper dataMapper;
 
-
+    @RequestMapping("/dept")
+    public List<Map> queryUserList(){
+        List<Map> dept = dataMapper.queryDeptList();
+        return dept;
+    }
 }
--- src/main/java/com/excel/mapper/DataMapper.java
+++ src/main/java/com/excel/mapper/DataMapper.java
@@ -1,6 +1,13 @@
 package com.excel.mapper;
 
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+import java.util.Map;
+
+@Mapper
 public interface DataMapper {
 
+    public List<Map> queryDeptList();
 
 }
