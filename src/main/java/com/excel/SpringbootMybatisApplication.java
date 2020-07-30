d5ff0f5019b39fe2570e75667dd50b668672f42e
WuMingCheng <452262448@qqq.com>
2020-07-30 11:36:49 +0800

    JSON时间格式化

--- src/main/java/com/excel/SpringbootMybatisApplication.java
+++ src/main/java/com/excel/SpringbootMybatisApplication.java
@@ -1,13 +1,22 @@
 package com.excel;
 
 import java.io.File;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.List;
+import java.util.TimeZone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.excel.util.ExcelUtil;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootApplication
 public class SpringbootMybatisApplication implements WebMvcConfigurer{
@@ -23,5 +32,19 @@ public class SpringbootMybatisApplication implements WebMvcConfigurer{
 		downloadRoot.deleteOnExit();
 		downloadRoot.mkdirs();
 		resourceHandlerRegistry.addResourceHandler("/download/**").addResourceLocations(downloadRoot.toURI().toString());
+	}
+	
+	@Bean
+    public ObjectMapper jacksonObjectMapperCustomization() {
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        TimeZone timeZone = TimeZone.getTimeZone("Asia/Shanghai");
+        dateFormat.setTimeZone(timeZone);
+        return new Jackson2ObjectMapperBuilder().timeZone(timeZone).dateFormat(dateFormat).build();
+    }
+	
+	@Override
+	public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+		converters.removeIf(converter -> converter instanceof MappingJackson2HttpMessageConverter);
+        converters.add(new MappingJackson2HttpMessageConverter(jacksonObjectMapperCustomization()));
 	}
 }
