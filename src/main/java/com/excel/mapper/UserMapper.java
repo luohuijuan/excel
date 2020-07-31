873df364fad37caa4597cc5a9185df01f74fe0b1
chenyi <776738764@qq.com>
2020-07-31 11:30:44 +0800

    页面样式

--- src/main/java/com/excel/mapper/UserMapper.java
+++ src/main/java/com/excel/mapper/UserMapper.java
@@ -3,6 +3,7 @@ package com.excel.mapper;
 import com.excel.domain.User;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+import sun.nio.cs.US_ASCII;
 
 import java.util.List;
 import java.util.Map;
@@ -13,4 +14,5 @@ public interface UserMapper {
     public List<Map> queryUserList();
 
     public  User findByNamePassword(@Param("username")String username , @Param("password")String password);
+
 }
